aeea66baf143960c30c598ceeda423b7da91d2cb
Francis Eyanae <franciseyanae2001@gmail.com>
2024-11-06 19:52:12 -0600

    Store Clerk users in database

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,13 +1,23 @@
 /* This is going to define our database tables and the types for them */
 /* This sets up the database structure */
 
-import { defineSchema, defineTable } from "convex/server"; /* These fns are used to create the structure of the database tables */
-import {v} from "convex/values"
+import {
+  defineSchema,
+  defineTable,
+} from "convex/server"; /* These fns are used to create the structure of the database tables */
+import { v } from "convex/values";
 
 /* This defines the basic "messages" table, which can hold text messages and the names of people who sent them.*/
-export default defineSchema ({
-  messages: defineTable({  /* A table named messages is created with two fields */
-    sender: v.string(),  /* Stores the name of the person sending the message, as a string */
-    content: v.string(), /* Stores the message content, also as a string */
-  })
-})
+export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"]),
+  messages: defineTable({
+    /* A table named messages is created with two fields */
+    sender:
+      v.string() /* Stores the name of the person sending the message, as a string */,
+    content: v.string() /* Stores the message content, also as a string */,
+  }),
+});
